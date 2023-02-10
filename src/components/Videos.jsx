1fbeb3b36289cb4008c12d26e1844e626e2aabf3
Ville Räsänen <ville.rasanen@hotmail.com>
2023-02-10 15:37:49 +0200

    youtubeclone

---
+++ src/components/Videos.jsx
@@ -0,0 +1,17 @@
+import { Stack, Box } from "@mui/system";
+import { VideoCard, ChannelCard } from "./";
+
+const Videos = ({ videos }) => {
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="flex-start" gap={2}>
+      {videos.map((item, index) => (
+        <Box key={index}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default Videos;
